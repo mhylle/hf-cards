993c5ebc040783b9d4e751b0554e84189353b56c
Martin Hylleberg <mhylle@yahoo.com>
2018-05-21 23:03:29 +0200

    sorted alphabetically added filter

--- src/app/cards/list-card/list-card.component.ts
+++ src/app/cards/list-card/list-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {CardService} from "../services/card.service";
 import {Card} from "../Card";
 import {ActivatedRoute, Router} from "@angular/router";
@@ -9,17 +9,44 @@ import {ActivatedRoute, Router} from "@angular/router";
   styleUrls: ['./list-card.component.css']
 })
 export class ListCardComponent implements OnInit {
+  letters: string[] = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z', 'Æ', 'Ø', 'Å', 'Alle'];
 
+  shownCards: Card[];
   cards: Card[];
-  constructor(private route: ActivatedRoute, private router: Router, private cardService: CardService) { }
+
+  constructor(private route: ActivatedRoute, private router: Router, private cardService: CardService) {
+  }
 
   ngOnInit() {
     this.cardService.getCards().subscribe(value => {
       this.cards = value;
+      this.shownCards = value;
+      this.shownCards.sort((a, b) => {
+        return a.firstname.localeCompare(b.firstname);
+      });
     })
   }
 
   gotoCard(id: string) {
     this.router.navigate(['/view-card', {id: id}]);
+  }
+
+  doFilter(letter: string) {
+    console.log(letter);
+    if (letter == 'Alle') {
+      this.shownCards = this.cards;
+      return;
+    }
+    this.shownCards = this.cards.filter(value => {
+      if (value.firstname.toLowerCase().startsWith(letter.toLowerCase())) {
+        return value;
+      }
+    });
+
+    for (let i = 0; i < this.shownCards.length; i++) {
+      let shownCard = this.shownCards[i];
+      console.log(shownCard.firstname);
+
+    }
   }
 }
